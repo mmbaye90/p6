218b64d05fa42afec05688c423ebc9a7493b34d0
Mbaye <mmbaye1@hotmail.fr>
2022-03-02 16:59:22 +0100

    création du schemas sauce puis exportation

---
+++ backend/models/sauceModel.js
@@ -0,0 +1,19 @@
+//Importation de mongoose
+const mongoose = require("mongoose");
+
+//Création du model sauce
+const sauceSchemas = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: false, default: 0 },
+  dislikes: { type: Number, required: false, default: 0 },
+  usersLiked: { type: [String], required: false },
+  usersDisliked: { type: [String], required: false },
+});
+
+module.exports = mongoose.model("Sauce", sauceSchemas);
