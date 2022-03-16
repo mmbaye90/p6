24a7bde79126f5812314de678a6001f3047c1e65
Mbaye <mmbaye1@hotmail.fr>
2022-03-16 12:11:38 +0100

    configuration du fichier multer opérée

--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,7 +1,7 @@
 //Importation du pckg multer
 const multer = require("multer");
 
-//configuration biblio de l'extension des fichiers
+//configuration dictionaire de l'extension des fichiers
 const MIME_TYPES = {
     "image/jpg": "jpg",
     "image/jpeg": "jpeg",
@@ -15,4 +15,15 @@ const storage = multer.diskStorage({
     destination: (req, file, callback) => {
         callback(null, "images"); // indique que les fichiers vont etre sauvegardés dans le dossier images
     },
+    filename: (req, file, callback) => {
+        //formatage du nom de fichier reçu par le frontend :on remplace les espaces par des _
+        const name = file.originalname.split(" ").join("_");
+        //définition de l'extension du fichier
+        const extension = MIME_TYPES[file.mimetype];
+        //Définition du chemin complet.Le null indique que tout s'est bien passé
+        callback(null, name + Date.now() + "." + extension);
+    },
 });
+
+//Exportation du fichier configuration avec la methode single qui gére uniquement les fichers images
+module.exports = multer({ storage: storage }).single("image");
