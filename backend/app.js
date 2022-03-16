7ad12917771f896fbb506aa58df11cfe6d6cfef2
Mbaye <mmbaye1@hotmail.fr>
2022-03-16 12:58:26 +0100

    Modification de app.js pour tenir en compte le fichier image

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,13 @@ require("./db/dbConfig");
 const userRouter = require("./routes/userRouter");
 const sauceRouter = require("./routes/sauceRouter");
 
+//Importation de path
+const path = require("path");
+
 /******************************************************      FIN IMPORTATATIONS  *****************************************************/
 /***************************************************** LES ENDPOINTS $ certains MIDDLEWARES  *************************************/
 app.use(express.json()); //me permet d'avoir accés au body de la requette
+app.use("/images", express.static(path.join(__dirname, "images"))); //indique à express de gérer la ressource imge de maniere static
 app.use(cors()); // palie à tous les problémes d'accés à mon web-api
 app.use("/api/auth", userRouter);
 app.use("/api/sauces", sauceRouter);
